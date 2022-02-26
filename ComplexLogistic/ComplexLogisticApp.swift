f736e798f5a7336defe980f24271c1d4b18aa64f
Amanda Chaudhary <amanda@catsynth-appwerx.com>
2022-02-26 09:52:15 -0800

    Initial Commit

---
+++ ComplexLogistic/ComplexLogisticApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComplexLogisticApp.swift
+//  ComplexLogistic
+//
+//  Created by Amanda Chaudhary on 2/26/22.
+//
+
+import SwiftUI
+
+@main
+struct ComplexLogisticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
